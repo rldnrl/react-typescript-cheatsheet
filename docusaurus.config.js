936425733981473c77a78a822dc84355b5fc2fbb
rldnrl <devpark1002@gmail.com>
2022-06-23 21:27:12 +0900

    feat: change edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,13 +36,13 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/rldnrl/react-typescript-cheatsheet"
+          editUrl: "https://github.com/rldnrl/react-typescript-cheatsheet/tree/main"
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/rldnrl/react-typescript-cheatsheet"
+          editUrl: "https://github.com/rldnrl/react-typescript-cheatsheet/tree/main"
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -63,9 +63,9 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'basic/intro',
             position: 'right',
-            label: 'Doc',
+            label: 'Docs',
           },
           // {to: '/blog', label: 'Blog', position: 'right'},
           {
